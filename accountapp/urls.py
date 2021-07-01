5eede5691acfd3a72684ab6555daa3ff6115bc84
Sinu <sin00528@gmail.com>
2021-07-01 17:22:57 +0900

    0701

---
+++ accountapp/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from accountapp.views import hello_world
+
+app_name = 'accountapp'
+
+urlpatterns = [
+    path('hello_world/', hello_world, name='hello_world'),
+]
+
